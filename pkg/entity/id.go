f0fedbce35da5a01e97964b81f8f44aa8297c987
Felipe Dias <felipedias.dev@gmail.com>
2023-04-04 09:28:15 -0300

    feat: :sparkles: add product entity

--- pkg/entity/id.go
+++ pkg/entity/id.go
@@ -2,13 +2,13 @@ package entity
 
 import "github.com/google/uuid"
 
-type ID uuid.UUID
+type ID = uuid.UUID
 
 func NewID() ID {
-	return ID(uuid.New())
+	return uuid.New()
 }
 
 func ParseID(s string) (ID, error) {
 	id, err := uuid.Parse(s)
-	return ID(id), err
+	return id, err
 }
